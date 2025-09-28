4a85de3be1172d78e984bdcb4099dd3427d1af51
jianghu608 <3335906691@qq.com>
2025-09-28 22:07:29 +0800

    增加了一种新的方法(format)

--- homework_01.py
+++ homework_01.py
@@ -1,16 +1,19 @@
 place = input('输入一个地名：')
 name = input('输入一个人名：')
 
-# 方法1
+# 方法1 
 print(f'{place}很美，{name}想去看看')
-
+   
 # 方法2
-print(place + '很美，' + name + '想去看看')
+print("{0}很美，{1}想去看看".format(place, name))
 
 # 方法3
-print(place, '很美，', name, '想去看看', sep='')
+print(place + '很美，' + name + '想去看看')
 
 # 方法4
+print(place, '很美，', name, '想去看看', sep='')
+
+# 方法5（不好）
 print(place,end = '')
 print('很美，', end = '')
 print(name, end = '')
