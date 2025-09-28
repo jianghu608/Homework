7ffac923edbc28c795bb4cae45bb644a56d0765b
jianghu608 <3335906691@qq.com>
2025-09-28 19:06:24 +0800

    print
    print及其参数等用法

--- homework_01.py
+++ homework_01.py
@@ -1,5 +1,17 @@
 place = input('输入一个地名：')
 name = input('输入一个人名：')
 
+# 方法1
 print(f'{place}很美，{name}想去看看')
+
+# 方法2
 print(place + '很美，' + name + '想去看看')
+
+# 方法3
+print(place, '很美，', name, '想去看看', sep='')
+
+# 方法4
+print(place,end = '')
+print('很美，', end = '')
+print(name, end = '')
+print('想去看看')
